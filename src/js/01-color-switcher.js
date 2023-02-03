583b9e6f5940f13e191b06cb1b83b1b4125f543d
OleksandrVaniuta <vaniutasa@gmail.com>
2023-02-03 22:17:13 +0200

    зробили задачу номер 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+const colorChanger = {
+  isActive: false,
+  changeCl: null,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.changeClr = setInterval(() => {
+      refs.startBtn.parentNode.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.changeClr);
+    this.isActive = false;
+  },
+};
+
+refs.startBtn.addEventListener('click', () => {
+  colorChanger.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  colorChanger.stop();
+});
